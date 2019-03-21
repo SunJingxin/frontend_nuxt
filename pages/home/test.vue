59fea8a9882f2c70825eda9007b5857b6fc604a4
sunjx <1825951386@qq.com>
2019-03-21 13:35:28 +0800

    -Ascss

---
+++ pages/home/test.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      s 2312
+        <div class="box">
+          <div class="f">23123</div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "test"
+    }
+</script>
+
+<style lang="scss" scoped>
+.box{
+  color: red;
+  .f{
+    color: fuchsia;
+  }
+}
+
+</style>
